4f8d322c9fb836b8b2d4023c0058c83ddb4a6b1b
lucreziascaffidi <lucrezia.scaffidi@gmail.com>
2024-10-26 16:03:53 +0200

    add: js comments on path to follow

---
+++ js/main.js
@@ -0,0 +1,23 @@
+console.log('Sono connesso');
+
+//Prendo il form dall'HTML
+
+// Dichiaro le variabili che mi servono con i dati che sono stati forniti nel testo della milestone
+// Variabile che contiene il prezzo orario per ciascun lavoro --> un oggetto che continene tutti i prezzi per ogni tipo di lavoro
+// Variabile che contiene i codici promozionali --> un array con i codici promozionali validi
+// Variabile sconto (25%)
+
+// Aggiungo l'evento al click di Submit al form
+// Devo prevenire il refresh della pagina
+// Accedere ai dati inseriti dall'utente 
+
+// Calcolo del preventivo base
+
+// Calcolo del preventivo con evenuale sconto se il codice è valido
+// Se il codice promozionale inserito non è valido far uscire un alert 
+
+// Formattare il prezzo finale in modo leggibile con due decimali
+
+// Farlo apparire nel DOM
+
+// Svuoltare i campi del form
