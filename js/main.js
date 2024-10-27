30ba92704684feb384586c341df4d4dc9b3cba7e
lucreziascaffidi <lucrezia.scaffidi@gmail.com>
2024-10-27 17:25:17 +0100

    feat: add logic on js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ console.log('Sono connesso');
 
 //Prendo il form dall'HTML
 const myForm = document.getElementById('my-form');
-console.log(myForm);
+//console.log(myForm);
 
 // Dichiaro le variabili che mi servono con i dati che sono stati forniti nel testo della milestone
+// Variabile che contiene le 10h richieste dal testo della milestone
+const workingHours = 10;
+
 // Variabile che contiene il prezzo orario per ciascun lavoro --> un oggetto che continene tutti i prezzi per ogni tipo di lavoro
 const hourPrice = {
     'backend-development': 20.50,
@@ -12,23 +15,80 @@ const hourPrice = {
     'project-analysis': 33.60
 };
 
+// console.log(hourPrice['backend-development']);
+// console.log(hourPrice['frontend-development']);
+// console.log(hourPrice['project-analysis']);
+
+
 // Variabile che contiene i codici promozionali --> un array con i codici promozionali validi
 const validPromoCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+// for (let i = 0; i < validPromoCodes.length; i++) {
+//     console.log(validPromoCodes[i]);
+// }
 
 // Variabile sconto (25%)
 const discountRate = 0.25;
 
 // Aggiungo l'evento al click di Submit al form
-// Devo prevenire il refresh della pagina
-// Accedere ai dati inseriti dall'utente 
+myForm.addEventListener('submit', function (event) {
 
-// Calcolo del preventivo base
+    // Devo prevenire il refresh della pagina
+    event.preventDefault();
 
-// Calcolo del preventivo con evenuale sconto se il codice è valido
-// Se il codice promozionale inserito non è valido far uscire un alert 
+    // Accedere ai dati inseriti dall'utente 
+    const userName = document.getElementById('user-name').value;
+    console.log(`Nome: ${userName}`);
 
-// Formattare il prezzo finale in modo leggibile con due decimali
+    const userSurname = document.getElementById('user-last-name').value;
+    console.log(`Cognome: ${userSurname}`);
 
-// Farlo apparire nel DOM
+    const userEmail = document.getElementById('user-email').value;
+    console.log(`Email: ${userEmail}`);
 
-// Svuoltare i campi del form
+    const userTextArea = document.getElementById('form-text-area').value;
+    console.log(`Text Area: ${userTextArea}`);
+
+    // Accedere alla scelta del tipo di lavoro da commissionare
+    const jobType = document.querySelector('select#job-request').value;
+    console.log(`Lavoro richiesto: ${jobType}`);
+
+    // Accedere al codice promozionale inserito dall'utente in modo pulito
+    const userPromoCode = document.getElementById('user-promo-code').value.trim().toUpperCase().replace(/[^A-Z0-9]/g, '');
+    console.log(`Codice Promozionale: ${userPromoCode}`);
+
+    // Calcolato preventivo base
+    let standardPrice = hourPrice[jobType] * workingHours
+    console.log(`Prezzo: ${standardPrice}€`);
+
+    // Creata una variabile che cambia nel tempo, inizialmente è uguale al prezzo standard e poi può essere modificata con lo sconto
+    let finalPrice = standardPrice
+
+    //Verifico il codice promozionale se è valido, quindi se è incluso nell'arrey validPromoCodes
+    // Calcolo del preventivo con evenuale sconto se il codice è valido
+    // Se il codice non è valido: alert che avvisa l'utente.
+    if (validPromoCodes.includes(userPromoCode)) {
+        finalPrice = standardPrice * (1 - discountRate);
+        console.log(`Prezzo scontato del 25%: ${finalPrice}€`);
+    } else if (userPromoCode) {
+        alert("Codice promozionale non valido");
+    }
+
+    // Formattare il prezzo finale in modo leggibile con due decimali
+    const formattedPrice = finalPrice.toFixed(2).replace(".", ",") + " €";
+    // console.log(formattedPrice);
+
+    // Aggiornare il prezzo finale nel DOM
+    const showPriceText = document.getElementById('final-price')
+    showPriceText.innerHTML = `€ ${formattedPrice}`
+
+    // Mostrarlo nel DOM 
+    const resultContainer = document.getElementById('result-container')
+
+    resultContainer.classList.remove('d-none');
+
+    // Svuoltare i campi del form
+    event.target.reset();
+
+})
+
